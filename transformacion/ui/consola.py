13cf85d48df298cc121dede8c797971f8eddc15f
jahlonteaching <jahlon@outlook.com>
2024-04-29 12:59:55 -0500

    Add project structure and finish implementation

--- transformacion/ui/consola.py
+++ transformacion/ui/consola.py
@@ -8,26 +8,42 @@ def main():
 
     metodo_elegido = int(input("Ingresa el número del método deseado: "))
 
-    if metodo_elegido in range(1, 5):
-        tipos_de_transformacion(metodo_elegido)
-    else:
-        print("Opción no válida. Por favor, elige un método del 1 al 4.")
+    transformador: Transformador = obtener_metodo_transformacion(metodo_elegido)
 
-def tipos_de_transformacion(metodo):
-    print(f"Has elegido el Método {metodo}.")
+    texto = input("Ingrese el texto a transformar")
+    tipo = tipos_de_transformacion()
+
+
+    texto_transformado = transformador.transformar(texto, tipo)
+
+    print(f"Texto transformado: {texto_transformado}")
+
+
+def tipos_de_transformacion():
     print("Por favor, selecciona un tipo de transformación:")
     print("1. Tipo de Transformación 1")
     print("2. Tipo de Transformación 2")
     print("3. Tipo de Transformación 3")
-    print("4. Tipo de Transformación 4")
 
     tipo_elegido = int(input("Ingresa el número del tipo de transformación deseado: "))
+    return tipo_elegido
 
-    if tipo_elegido in range(1, 5):
-        # Aquí puedes implementar la lógica específica para cada tipo de transformación.
-        print(f"Has elegido el Tipo de Transformación {tipo_elegido}.")
-    else:
-        print("Opción no válida. Por favor, elige un tipo del 1 al 4.")
+
+def obtener_metodo_transformacion(metodo: int) -> Transformador
+    if metodo == 1:
+        pass
+
+    if metodo == 2:
+        pass
+
+    if metodo == 3:
+        pass
+
+    if metodo == 4:
+        pass
+
+    raise ValueError
+
 
 if __name__ == "__main__":
     main()
