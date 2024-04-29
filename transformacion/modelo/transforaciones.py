8a1562e300efc2720031280c92f8fdd8c0b87cb4
T-cordoba <tommy.cormo@gmail.com>
2024-04-29 12:58:47 -0500

    changed methods

--- transformacion/modelo/transforaciones.py
+++ transformacion/modelo/transforaciones.py
@@ -2,18 +2,18 @@ class MetodoT(Transformador):
 
     def transformar(self, string:str, tipo: int):
         if tipo == 1:
-            return self.metodo1(string)
+            return self.fillspaces(string)
         elif tipo == 2:
-            return self.metodo2(string)
+            return self.mayus(string)
         elif tipo == 3:
-            return self.metodo3(string)
+            return self.hyphentospaces(string)
 
 
-def metodo1(string:str):
+def fillspaces(string:str):
     return string.replace(" ", "-")
 
-def metodo2(string:str):
-    return " ".join(string)
-
-def metodo3(string:str):
+def mayus(string:str):
     return string.upper()
+
+def hyphentospaces(string:str):
+    return string.replace("-", " ")
